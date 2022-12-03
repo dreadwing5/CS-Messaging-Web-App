f5a955b00621060fad32e99beb8227deac425031
dreadwing5 <sachinspyk@outlook.com>
2022-12-04 00:16:25 +0530

    added routes for users, message and conversation

---
+++ controllers/authController.js
@@ -0,0 +1,114 @@
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const verfiyAsync = promisify(jwt.verify);
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+const cookieOptions = {
+  expires: new Date(
+    Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+  ),
+  httpOnly: true, //don't let browser js access this cookie, used to prevent xss attacks
+};
+
+if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    role: req.body.role,
+  });
+
+  const token = signToken(newUser._id);
+  res.cookie('jwt', token, cookieOptions);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      user: newUser,
+    },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email } = req.body;
+  // 1) Check if email and password exist
+
+  if (!email) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    // 401 = Unauthorized
+    return next(new AppError('Incorrect email or password', 401));
+  }
+  // 3) Generate JWT and send it back to the client
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check if it;s there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access', 401)
+    );
+  }
+
+  // 2) Verification of token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // 3) Check if user exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError('The user belonging to the token does not exist', 401)
+    );
+  }
+
+  // 4) Check if user changed password after the token was issued
+
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please log in again', 401)
+    );
+  }
+
+  // GRANT ACCESS TO PROTECTED ROUTE
+  req.user = currentUser;
+  next();
+});
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    //403 = Forbidden
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403)
+      );
+    }
+
+    next();
+  };
+};
